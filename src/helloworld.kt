b7a1f0f7146556f4fd965a1a43f1978d478bbbdf
sivajik <sivaji_kondapalli@yahoo.com>
2021-06-02 21:43:15 +0100

    few changes

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -2,4 +2,5 @@ fun main(args: Array<String>) {
 	println("welcome to kotlin programming language...");
 	println("another line to check git connectivity..");
 	println("last time check for git..");
+	println("last time check for git..");	
 }
