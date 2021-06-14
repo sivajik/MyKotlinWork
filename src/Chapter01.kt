df65c24e6fbb650d0503618d7fd7c82045fa61fa
sivajik <sivaji_kondapalli@yahoo.com>
2021-06-14 21:06:41 +0100

    chapter 01 - completed

---
+++ src/Chapter01.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+	// when used if as an expression 'else' clause is mandatory.
+	var x = 42;
+	println(if (x < 50) "the given value is less than 50" else "given value is greater than 50");
+}
