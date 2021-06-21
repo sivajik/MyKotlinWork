89a181c6e22ebb0ab6af33030cd96c17d21fabdf
sivajik <sivaji_kondapalli@yahoo.com>
2021-06-21 22:16:18 +0100

    Rock Paper Scissors

--- src/Chapter03.kt
+++ src/Chapter03.kt
@@ -1,4 +1,56 @@
 fun main(args: Array<String>) {
-	println("Given args: " + args.size);
-	println("first arg = " + args[0]);
+	var options = arrayOf("Rock", "Paper", "Scissors");
+	var gameChoice: String = getGameChouse(options);
+	println("Computer has selected : " + gameChoice);
+
+	var userChoice = getUserChoice(options);
+	println("User has selected : " + userChoice);
+
+	println(printResult(gameChoice, userChoice));
+}
+
+
+fun getGameChouse(input: Array<String>): String {
+	var index = Math.random() * input.size;
+	println("Index: " + index.toInt());
+	return input[index.toInt()];
+}
+
+fun getUserChoice(options: Array<String>): String {
+	println("Please select one of the following inputs: ");
+
+	for (item in options) {
+		println(item);
+	}
+
+	var validChoice = false;
+	var selection: String = "";
+	while (!validChoice) {
+		var input = readLine();
+		for (item in options) {
+			if (input.equals(item)) {
+				validChoice = true;
+				selection = input.toString();
+			}
+		}
+
+		if (!validChoice) {
+			println("*** Given input is wrong.. Try again.");
+		}
+	}
+	return selection;
+}
+
+fun printResult(gameChoice: String, userChoice: String): String {
+	var result: String = "";
+	if (gameChoice == userChoice) {
+		result = "Tie";
+	} else if ((gameChoice == "Scissors" && userChoice == "Rock") || (gameChoice == "Rock" && userChoice ==
+				"Paper") || (gameChoice == "Paper" && userChoice == "Scissors")
+	) {
+		result = "You Win";
+	} else {
+		result = "You Lose";
+	}
+	return result;
 }
