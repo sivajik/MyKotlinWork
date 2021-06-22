8bde333a2db926da98ecd76704f98f89666d859b
sivajik <sivaji_kondapalli@yahoo.com>
2021-06-22 21:29:56 +0100

    c4

--- src/Chapter04.kt
+++ src/Chapter04.kt
@@ -2,21 +2,25 @@ fun main(args: Array<String>) {
 	var d1: Dog = Dog("BowBow1", 25, "Labrador1");
 	var d2: Dog = Dog("BowBow2", 125, "Labrador2");
 	var d3: Dog = Dog("BowBow3", 225, "Labrador3");
-	
+
 	d1.bark();
 	println(d1);
 	println("====")
 	var dogs: Array<Dog> =
-		arrayOf(Dog("brownie1", 14, "daburman1"),
+		arrayOf(
+			Dog("brownie1", 14, "daburman1"),
 			Dog("brownie2", 114, "daburman2"),
-			Dog("brownie3", 214, "daburman3"))
+			Dog("brownie3", 214, "daburman3")
+		)
 
 	for (dog: Dog in dogs) {
 		println(dog);
 	}
 	println("Another way...");
-	for (i in 0 until dogs.size) {
-		println(dogs[i])
+
+	var newDogs: Array<Dog> = arrayOf(d1, d2, d3);
+	for (i in 0 until newDogs.size) {
+		println(newDogs[i])
 	}
 }
 
