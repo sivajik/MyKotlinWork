819d1cbe83651847ee1bb048d530b9060e7fc2af
sivajik <sivaji_kondapalli@yahoo.com>
2021-06-24 22:02:58 +0100

    c5

--- src/Chapter05.kt
+++ src/Chapter05.kt
@@ -1,3 +1,30 @@
 fun main(args: Array<String>) {
 	println("hello wolrd");
 }
+
+open class Animal() {
+	open val image = "";
+	open val food = "";
+	open val habitat = "";
+	/* I am not keeping as open this */ var hunger = 10;
+
+	open fun makeNoise() {
+		println("Animal is making some noise");
+	}
+
+	open fun eat() {
+		println("Animal is eating");
+	}
+
+	open fun roam() {
+		println("Animal is roaming");
+	}
+
+	fun sleep() {
+		println("Animal is sleeping");
+	}
+}
+
+class Hippo : Animal() {
+
+}
