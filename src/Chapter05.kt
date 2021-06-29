6125c9f539e7118af127f7c17d37079b57987598
sivajik <sivaji_kondapalli@yahoo.com>
2021-06-29 21:25:03 +0100

    c5

--- src/Chapter05.kt
+++ src/Chapter05.kt
@@ -27,11 +27,24 @@ open class Animal() {
 
 
 open class Canine : Animal() {
-
+	override fun roam() {
+		println("The Canine is roaming");
+	}
 }
 
 class Wolf : Canine() {
+	override val image = "wolf.jpeg";
+	override val food = "meat";
 
+	override val habitat = "forests";
+
+	override fun makeNoise() {
+		println("Howl!!!!");
+	}
+
+	override fun eat() {
+		println("The Wolf is eating food.");
+	}
 }
 
 class Hippo : Animal() {
@@ -39,4 +52,12 @@ class Hippo : Animal() {
 	override val food = "grass";
 
 	override val habitat = "water";
+
+	override fun makeNoise() {
+		println("Grunt!!!!");
+	}
+
+	override fun eat() {
+		println("The Hippo is eating food.");
+	}
 }
